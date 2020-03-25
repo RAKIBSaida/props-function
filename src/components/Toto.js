cbb0d3994bb433d14ab52b0bc56fbb1663e1f35c
RAKIBSaida <rakibsaida@gmail.com>
2020-03-25 14:34:26 +0000

    utiliser des fonctions dans les props

---
+++ src/components/Toto.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Toto=(props)=>{
+   // console.log(props.leState.MessageMaman);
+   //console.log(props);
+ const btnReponseToto=props.leState.messageMaman !== null ?( <button onClick={props.reponseDeToto}>Réponse</button>):( <button disabled>Réponse</button>)
+return (
+    <div>
+        <h2>{props.name}</h2>
+        {btnReponseToto}
+        <p>{props.leState.messageToto}</p>
+    </div>
+)
+}
+export default Toto;
